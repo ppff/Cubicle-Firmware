4250377b4f8adf71b476d0809f62719e21c8e4f6
Florian Fontan <florian.fontan@ensimag.grenoble-inp.fr>
2015-05-20 13:53:41 +0200

    Add file defining cube constants

---
+++ cubicle/Inc/constant.h
@@ -0,0 +1,11 @@
+/*
+ * If you want to use a LENGTH >= 16 LEDs, you will have to change to type used
+ * for the "data" field in the LED structure (led.h/led.c) from uint16_t to
+ * uint32_t or more.
+ */
+#define LENGTH 9
+
+#define WIDTH  9
+
+#define HEIGHT 9
+
