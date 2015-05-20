b58dadeef45358db12c6541b81f2018b26996a37
Florian Fontan <florian.fontan@ensimag.grenoble-inp.fr>
2015-05-20 16:42:50 +0200

    Update comment

--- cubicle/Inc/constant.h
+++ cubicle/Inc/constant.h
@@ -1,7 +1,6 @@
 /*
- * If you want to use a LENGTH >= 16 LEDs, you will have to change to type used
- * for the "data" field in the LED structure (led.h/led.c) from uint16_t to
- * uint32_t or more.
+ * If you want to use a LENGTH >= 16 LEDs, you will have to update the type
+ * definition of "line_t" in led.h to uint32_t or more.
  */
 #define LENGTH 9
 
