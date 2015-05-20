df0709e68c49921382a287b1066695e530a56de8
Florian Fontan <florian.fontan@ensimag.grenoble-inp.fr>
2015-05-20 13:56:24 +0200

    Partially implement LED functions

---
+++ cubicle/Src/led.c
@@ -0,0 +1,87 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "led.h"
+#include "constant.h"
+
+/**
+ * 0000000111111111 0000000110100101 ... <= plane 0
+ * %%%%%%% line 0   %%%%%%% line 1
+ * 0000000100000001 0000000110101101 ... <= plane 1
+ *                ^
+ *    LED 0 of line 0 of plane 1
+ * ...
+ *
+ * %: unused bits.
+ */
+struct led {
+	uint16_t data[HEIGHT][WIDTH];
+};
+
+bool in_range(int x, int y, int z)
+{
+	if (
+			   x >= 0 && x < LENGTH
+			&& y >= 0 && y < WIDTH
+			&& z >= 0 && z < HEIGHT) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/**
+ * line: 0000010001
+ * mask: 0000000100
+ * or:   0000010101 <= LED on
+ */
+void led_switch_on(struct led *l, int x, int y, int z)
+{
+	if (in_range(x,y,z))
+		l->data[z][x] |= 1 << y;
+}
+
+/**
+ * line: 0000010101
+ * mask: 0000000100
+ * xor:  0000010001 <= LED off
+ */
+void led_switch_off(struct led *l, int x, int y, int z)
+{
+	if (in_range(x,y,z))
+		l->data[z][x] ^= 1 << y;
+}
+
+void led_reset(struct led *l)
+{
+	memset(l, 0, WIDTH*HEIGHT*sizeof(uint16_t));
+}
+
+/*
+
+void push_if(int condition)
+{
+	if (condition) {
+		// push 1
+	} else {
+		// push 0
+	}
+}
+
+void led_display(struct led *l)
+{
+	for (int k=0; k<HEIGHT; k++) {
+		for (int l=0; k<HEIGHT; l++)
+			push_if(l == k);
+		for (int j=0; j<WIDTH; j++) {
+			uint16_t tmp = l->data[k][j];
+			for (int l=0; l<LENGTH; l++) {
+				push_if(tmp && 0b1);
+				tmp <<= 1;
+			}
+		}
+	}
+}
+
+*/
