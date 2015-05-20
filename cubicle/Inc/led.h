15f1c1570fb4960ca8efe02fa95de5cda29f3063
Florian Fontan <florian.fontan@ensimag.grenoble-inp.fr>
2015-05-20 10:01:40 +0200

    Create header for led management

---
+++ cubicle/Inc/led.h
@@ -0,0 +1,25 @@
+/**
+ * Structure representing the LEDs of the cube.
+ */
+struct led;
+
+/**
+ * Switch on the LED (x,y,z).
+ */
+void led_switch_on(struct led *l, int x, int y, int z);
+
+/**
+ * Switch off the LED (x,y,z).
+ */
+void led_switch_off(struct led *l, int x, int y, int z);
+
+/**
+ * Switch off all LEDs.
+ */
+void led_reset(struct led *l);
+
+/**
+ * Display the LED structure on the cube.
+ */
+void led_display(struct led *l);
+
