82fc7cb64565ae1b24c4c09ea195503476adeca1
Florian Fontan <florian.fontan@ensimag.grenoble-inp.fr>
2015-05-22 10:50:54 +0200

    Change to dynamic structure

--- cubicle/Inc/CUB_Display.h
+++ cubicle/Inc/CUB_Display.h
@@ -2,7 +2,6 @@
 #define LED_H
 
 #include <stdint.h>
-#include "constant.h"
 
 /**
  * Type used to store the content of a line. It must verify:
@@ -13,32 +12,49 @@ typedef uint16_t line_t;
 /**
  * Structure representing the LEDs of the cube.
  *
+ * The structure must be initialized and freed.
+ *
+ * LED description:
  * %%%%%%% line 0   %%%%%%% line 1
  * 0000000111111111 0000000110100101 ... <= plane 0
  * 0000000100000001 0000000110101101 ... <= plane 1
  *                ^
  *    LED 0 of line 0 of plane 1
  * ...
- *
  * %: unused bits.
  *
  * The content of the "data" field will be copied in "buffer" when updating
  * display,
  */
 struct led {
-	line_t   data[HEIGHT][WIDTH];
-	line_t buffer[HEIGHT][WIDTH];
+	line_t **data;
+	line_t **buffer;
+	uint32_t length;
+	uint32_t width;
+	uint32_t height;
 };
+
+/**
+ * Initialize a LED structure.
+ */
+void led_init(struct led *l, uint32_t length, uint32_t width, uint32_t height);
+
+/**
+ * Free a LED structure.
+ */
+void led_free(struct led **pl);
 
 /**
  * Switch on the LED (x,y,z).
  */
-void led_switch_on(struct led *l, int x, int y, int z);
+void led_switch_on(struct led *l, uint32_t x, uint32_t y, uint32_t z);
 
 /**
  * Switch off the LED (x,y,z).
  */
-void led_switch_off(struct led *l, int x, int y, int z);
+void led_switch_off(struct led *l, uint32_t x, uint32_t y, uint32_t z);
+
+void CUB_translate(struct led *l, int32_t x, int32_t y, int32_t z);
 
 /**
  * Switch off all LEDs.
@@ -53,6 +69,6 @@ void update_display(struct led *l);
 /**
  * Return 1 if LED (x,y,z) is on, else 0.
  */
-int led_get(struct led *l, int x, int y, int z);
+int led_get(struct led *l, uint32_t x, uint32_t y, uint32_t z);
 
 #endif
--- cubicle/Src/CUB_Display.c
+++ cubicle/Src/CUB_Display.c
@@ -1,14 +1,37 @@
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #include "CUB_Display.h"
 
-bool in_range(int x, int y, int z)
+void led_init(struct led *l, uint32_t length, uint32_t width, uint32_t height)
 {
-	if (
-			   x >= 0 && x < LENGTH
-			&& y >= 0 && y < WIDTH
-			&& z >= 0 && z < HEIGHT) {
+	l->length = length;
+	l->width  = width;
+	l->height = height;
+
+	l->data   = malloc(sizeof(line_t *) * height);
+	l->buffer = malloc(sizeof(line_t *) * height);
+	for (uint32_t k=0; k<height; ++k) {
+		l->data[k]   = malloc(sizeof(line_t) * width);
+		l->buffer[k] = malloc(sizeof(line_t) * width);
+	}
+}
+
+void led_free(struct led **pl)
+{
+	struct led *l = *pl;
+	for (uint32_t k=0; k<l->height; ++k) {
+		free(l->data[k]);
+		free(l->buffer[k]);
+	}
+	free(l->data);
+	free(l->buffer);
+}
+
+bool in_range(struct led *l, uint32_t x, uint32_t y, uint32_t z)
+{
+	if (x < l->length && y < l->width && z < l->height) {
 		return true;
 	} else {
 		return false;
@@ -20,9 +43,9 @@ bool in_range(int x, int y, int z)
  * mask: 0000000100
  * or:   0000010101 <= LED on
  */
-void led_switch_on(struct led *l, int x, int y, int z)
+void led_switch_on(struct led *l, uint32_t x, uint32_t y, uint32_t z)
 {
-	if (in_range(x,y,z))
+	if (in_range(l, x, y, z))
 		l->data[z][y] |= 1 << x;
 }
 
@@ -31,21 +54,25 @@ void led_switch_on(struct led *l, int x, int y, int z)
  * mask: 0000000100
  * xor:  0000010001 <= LED off
  */
-void led_switch_off(struct led *l, int x, int y, int z)
+void led_switch_off(struct led *l, uint32_t x, uint32_t y, uint32_t z)
 {
-	if (in_range(x, y, z))
+	if (in_range(l, x, y, z))
 		if (l->data[z][y] & (1 << x))
 			l->data[z][y] ^= 1 << x;
 }
 
-int led_get(struct led *l, int x, int y, int z)
+int led_get(struct led *l, uint32_t x, uint32_t y, uint32_t z)
 {
-	return ((l->data[z][y] >> x) & 1);
+	if (in_range(l, x, y, z)) {
+		return ((l->data[z][y] >> x) & 1);
+	} else {
+		return 0;
+	}
 }
 
 void clear(struct led *l)
 {
-	memset(l, 0, WIDTH*HEIGHT*sizeof(line_t));
+	memset(l, 0, l->width*l->height*sizeof(line_t));
 }
 
 void push_if(int condition)
@@ -59,16 +86,16 @@ void push_if(int condition)
 
 void update_display(struct led *l)
 {
-	for (int k=0; k<HEIGHT; k++)
-		memcpy(l->buffer[k], l->data[k], sizeof(line_t)*WIDTH);
+	for (uint32_t k=0; k<l->height; k++)
+		memcpy(l->buffer[k], l->data[k], sizeof(line_t)*l->width);
 
 	// while...
-	for (int k=0; k<HEIGHT; k++) {
-		for (int l=0; k<HEIGHT; l++)
-			push_if(l == k);
-		for (int j=0; j<WIDTH; j++) {
+	for (uint32_t k=0; k<l->height; k++) {
+		for (uint32_t n=0; n<l->height; n++)
+			push_if(n == k);
+		for (uint32_t j=0; j<l->width; j++) {
 			line_t tmp = l->buffer[k][j];
-			for (int l=0; l<LENGTH; l++) {
+			for (uint32_t n=0; n<l->length; n++) {
 				push_if(tmp && 0b1);
 				tmp <<= 1;
 			}
