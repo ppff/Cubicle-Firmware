47ab58906f5494b52ac09755826ce96abdbf5b87
Florian Fontan <florian.fontan@ensimag.grenoble-inp.fr>
2015-05-22 13:41:18 +0200

    Update LED tests

--- cubicle/Src/led_test.c
+++ cubicle/Src/led_test.c
@@ -1,23 +1,15 @@
 #include <assert.h>
-#include "led.h"
+#include "CUB_Display.h"
 
 int main(void)
 {
 	struct led l;
-	led_switch_on(&l, 1, 2, 3);
-	assert(led_get(&l, 1, 2, 3) != 0);
-
-	led_reset(&l);
-	led_switch_on(&l, 1, 2, 3);
-	led_switch_on(&l, 1, 2, 3);
-	assert(led_get(&l, 1, 2, 3) != 0);
-
-	led_reset(&l);
-	led_switch_on(&l, 1, 2, 3);
-	led_switch_off(&l, 1, 2, 3);
-	led_switch_off(&l, 2, 3, 4);
-	assert(led_get(&l, 1, 2, 3) == 0);
-	assert(led_get(&l, 2, 3, 4) == 0);
+	led_init(&l, 9, 8, 10);
+	led_switch_on(&l, 0, 0, 0);
+	led_project(&l, 0, 0);
+	led_project(&l, 1, 0);
+	led_project(&l, 2, 0);
+	led_free(&l);
 
 	return 0;
 }
