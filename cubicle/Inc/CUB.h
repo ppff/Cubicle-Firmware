564fed31510435cd20864dd4247dba95106f1e26
Daniel Braikeh <danbraik@hotmail.com>
2015-05-26 14:26:38 +0200

    Add main CUB module

---
+++ cubicle/Inc/CUB.h
@@ -0,0 +1,29 @@
+#ifndef _CUB_H_
+#define _CUB_H_
+
+/**
+ * Entry point of the application.
+ * Should be redefined by the user.
+ */
+void CUB_ApplicationRun();
+
+#include "event/CUB_event.h"
+#include "text/CUB_text.h"
+
+
+/**
+ * Initialize Cubicle
+ */
+void CUB_Init();
+
+/**
+ * Free Cubicle ressources
+ */
+void CUB_Quit();
+
+/**
+ * Sleep for milliseconds
+ */
+void CUB_Sleep(uint32_t milli);
+
+#endif
