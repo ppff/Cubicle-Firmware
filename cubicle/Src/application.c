ea83804e516bfa789cbaee47c9350b60d8da37b0
Daniel Braikeh <danbraik@hotmail.com>
2015-05-28 17:29:41 +0200

    Add explicit event test with LCD messages

--- cubicle/Src/application.c
+++ cubicle/Src/application.c
@@ -18,7 +18,55 @@ void CUB_ApplicationRun()
 			if (event.type == CUB_BUTTON_PRESSED) {
 				switch (event.button.id) {
 					case CUB_BTN_UP:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("UP pressed");
 						HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, 1);
+						break;
+					case CUB_BTN_DOWN:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("DOWN pressed");
+						break;
+					case CUB_BTN_LEFT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("LEFT pressed");
+						break;
+					case CUB_BTN_RIGHT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("RIGHT pressed");
+						break;
+					case CUB_BTN_TOP:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("TOP pressed");
+						break;
+					case CUB_BTN_BOTTOM:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("BOTTOM pressed");
+						break;
+					case CUB_BTN_M_LEFT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("M_LEFT pressed");
+						break;
+					case CUB_BTN_M_RIGHT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("M_RIGHT pressed");
+						break;
+					case CUB_BTN_SM_LEFT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("SM_LEFT pressed");
+						break;
+					case CUB_BTN_SM_RIGHT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("SM_RIGHT pressed");
 						break;
 					default:
 						;
@@ -26,8 +74,56 @@ void CUB_ApplicationRun()
 			} else if (event.type == CUB_BUTTON_RELEASED) {
 				switch (event.button.id) {
 					case CUB_BTN_UP:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("UP released");
 						HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, 0);
 						count++;
+						break;
+					case CUB_BTN_DOWN:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("DOWN released");
+						break;
+					case CUB_BTN_LEFT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("LEFT released");
+						break;
+					case CUB_BTN_RIGHT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("RIGHT released");
+						break;
+					case CUB_BTN_TOP:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("TOP released");
+						break;
+					case CUB_BTN_BOTTOM:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("BOTTOM released");
+						break;
+					case CUB_BTN_M_LEFT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("M_LEFT released");
+						break;
+					case CUB_BTN_M_RIGHT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("M_RIGHT released");
+						break;
+					case CUB_BTN_SM_LEFT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("SM_LEFT released");
+						break;
+					case CUB_BTN_SM_RIGHT:
+						CUB_TextHome();
+						CUB_TextClear();
+						CUB_TextPrint("SM_RIGHT released");
 						break;
 					default:
 						;
