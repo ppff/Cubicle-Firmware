5ec832d32213e05d4dc8e56953a9c7c473969f99
Daniel Braikeh <danbraik@hotmail.com>
2015-06-05 10:51:11 +0200

    Add CUB_api.h
    Define available api commands

---
+++ cubicle/Inc/api/CUB_api.h
@@ -0,0 +1,26 @@
+#ifndef _CUBICLE_API_H_
+#define _CUBICLE_API_H_
+
+/**
+ * API commands
+ */
+typedef enum {
+	CUB_API_NO_CMD = 0,
+	CUB_API_GET_VERSION,
+
+	CUB_API_CLEAR_LEDS,
+	CUB_API_LED_ON,
+	CUB_API_LED_OFF,
+	CUB_API_UP_LED_DISPLAY,
+	
+	CUB_API_CLEAR_LCD,
+	CUB_API_SET_CURSOR,
+	CUB_API_PRINT,
+	// ...
+	
+	CUB_API_PUSH_EVENT,
+
+	CUB_API_NB_COMMANDS // internal use
+} CUB_ApiCommand;
+
+#endif
