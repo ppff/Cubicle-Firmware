f6e33444a0107389574b328e4fead3bcf213eace
Daniel Braikeh <danbraik@hotmail.com>
2015-06-05 12:22:05 +0200

    Implement API fcts. Add (u)integers getters

--- cubicle/Inc/api/CUB_api.h
+++ cubicle/Inc/api/CUB_api.h
@@ -35,6 +35,18 @@ typedef enum {
 } CUB_ApiCommand;
 
 /**
+ * Initialiaze the API module
+ */
+void CUB_ApiInit();
+
+/**
+ * Is API started ?
+ * The API is started when we 
+ * receive our first command
+ */
+bool CUB_ApiIsStarted();
+
+/**
  * Pull the next data as api command.
  * It returns 'true' when there is a cmd
  * to treat. Else 'false'.
@@ -42,5 +54,41 @@ typedef enum {
  * is copied into that memory area.
  */
 bool CUB_ApiGetNextCommand(CUB_ApiCommand * cmd);
+
+/**
+ * Pull the next data as data for previous
+ * command.
+ */
+uint8_t CUB_ApiGetNextUint8();
+
+/**
+ * Pull the next data as data for previous
+ * command.
+ */
+int8_t CUB_ApiGetNextInt8();
+
+/**
+ * Pull the next data as data for previous
+ * command.
+ */
+uint16_t CUB_ApiGetNextUint16();
+
+/**
+ * Pull the next data as data for previous
+ * command.
+ */
+int16_t CUB_ApiGetNextInt16();
+
+/**
+ * Pull the next data as data for previous
+ * command.
+ */
+uint32_t CUB_ApiGetNextUint32();
+
+/**
+ * Pull the next data as data for previous
+ * command.
+ */
+int32_t CUB_ApiGetNextInt32();
 
 #endif
