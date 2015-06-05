c9aa4052ea94fbc5b41571d6a90bf8a7e4714d86
Daniel Braikeh <danbraik@hotmail.com>
2015-06-05 11:16:02 +0200

    Clean API app

--- cubicle/Src/applications/api_rt/api_rt.c
+++ cubicle/Src/applications/api_rt/api_rt.c
@@ -2,18 +2,16 @@
 
 void apiRealTimeRun()
 {
-	CUB_EnableButtonRepeat(700, 80);
+	//CUB_EnableButtonRepeat(700, 80);
 
 	CUB_LEDs_clear();
 	CUB_TextClear();
 	CUB_TextHome();
-	CUB_TextPrint("toto");
+	CUB_TextPrint("API ready");
 	CUB_ApiCommand cmd;
 	CUB_Event event;
-	int loop = 0;
 	/* Infinite loop */
 	for(;;) {
-
 
 //		while ((cmd = CUB_ApiGetNextCommand()) != CUB_API_NO_CMD) {
 //			switch (cmd) {
@@ -30,48 +28,34 @@ void apiRealTimeRun()
 
 		while (CUB_PollEvent(&event)) {
 			if (event.type == CUB_BUTTON_PRESSED) {
-				if (status_update(event.button.id))
-					CUB_ApplicationRun_snake();
 				switch (event.button.id) {
-					case CUB_BTN_UP:
-						pattern_display_update(-1, 0, 0);
-						break;
-					case CUB_BTN_DOWN:
-						pattern_display_update(1, 0, 0);
-						break;
-					case CUB_BTN_LEFT:
-						pattern_display_update(0, -1, 0);
-						break;
-					case CUB_BTN_RIGHT:
-						pattern_display_update(0, 1, 0);
-						break;
-					case CUB_BTN_TOP:
-						pattern_display_update(0, 0, 1);
-						break;
-					case CUB_BTN_BOTTOM:
-						pattern_display_update(0, 0, -1);
-						break;
-					case CUB_BTN_M_LEFT:
-						application_update(PREV_GROUP);
-						break;
-					case CUB_BTN_M_RIGHT:
-						application_update(NEXT_GROUP);
-						break;
-					case CUB_BTN_SM_LEFT:
-						application_update(PREV_PATTERN);
-						break;
-					case CUB_BTN_SM_RIGHT:
-						application_update(NEXT_PATTERN);
-						break;
-					default:
-						;
+				case CUB_BTN_UP:
+					break;
+				case CUB_BTN_DOWN:
+					break;
+				case CUB_BTN_LEFT:
+					break;
+				case CUB_BTN_RIGHT:
+					break;
+				case CUB_BTN_TOP:
+					break;
+				case CUB_BTN_BOTTOM:
+					break;
+				case CUB_BTN_M_LEFT:
+					break;
+				case CUB_BTN_M_RIGHT:
+					break;
+				case CUB_BTN_SM_LEFT:
+					break;
+				case CUB_BTN_SM_RIGHT:
+					break;
+				default:
+					;
 				}
 			}
-			// Cech if SD card is still in slot.
 		}
 
 		CUB_Sleep(50);
-
-		loop++;
 	}
 }
+
