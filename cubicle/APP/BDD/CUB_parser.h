b43a186d69ac0b6574ad4725d97661ba9da89d0b
Yassine El Khadiri <yassine.el-khadiri@ensimag.grenoble-inp.fr>
2015-07-23 18:32:57 +0200

    clean up unnecessary modules

--- cubicle/APP/BDD/CUB_parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef CUB_PARSER_H
-#define CUB_PARSER_H
-
-#include "CUB_parser_def.h"
-#define MAX_SUPPORTED_VERSION 1
-
-int CUB_parser_parse_file(void* pf, void* file);
-
-#endif
--- cubicle/APP/BDD/CUB_parser_def.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#include <stdbool.h>
-
-bool CUB_parser_get_next_line(char buff[64], void *arg);
-
-void CUB_parser_list_add(void *list, uint32_t x, uint32_t y, uint32_t z);
-
